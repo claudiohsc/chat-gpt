23f4187dd625596287cbfc0578157c51122f9cfd
CLaudio Henrique <claudio885henrique@gmail.com>
2023-01-19 08:28:22 -0300

    config inputs

--- main.py
+++ main.py
@@ -10,20 +10,20 @@ while True:
     print('Bem vindo(a) ao Chat, para parar digite: "stop".')
     print(' ')
 
-    ask = input('Pergunte o que quiser: ')
-    if ask == 'stop':
+    pergunta = input('Pergunte o que quiser: ')
+    if pergunta == 'stop':
         break
 
     response = openai.Completion.create(
 
     model="text-davinci-003",
-    prompt=ask,
+    prompt=pergunta,
     temperature=0,
     max_tokens=100,
     top_p=1,
     frequency_penalty=0.0,
     presence_penalty=0.0,
-    stop=[" Human:", " AI:"]
+    stop=[" "]
     )
 
     text = response['choices'][0]['text']
